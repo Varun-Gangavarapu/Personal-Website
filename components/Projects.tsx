f5187bfa34cfbf045a1c7eedd1e5b92b4daff00f
varungangavarapu <vaarunsaigangavarapu@gmail.com>
2024-05-21 11:03:42 -0400

    Updated photo and resume

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -23,32 +23,36 @@ const Projects = () => {
                 <Image
                   className="w-full h-full object-contain rounded-lg"
                   src={project1}
-                  alt="Project 1 Image"
+                  alt="Vector Database Image"
                 />
-              <div className="hidden lgl:inline-block absolute w-full h-full bg-imgCover/40 rounded-lg top-0 left-0 group-hover:bg-transparent duration-300"></div>
-
+                <div className="hidden lgl:inline-block absolute w-full h-full bg-imgCover/40 rounded-lg top-0 left-0 group-hover:bg-transparent duration-300"></div>
               </div>
             </a>
             <div className="w-full h-4/5 mt-7 xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right xl:-ml-16 z-10">
               <p className="font-titleFont text-textGreen text-sm tracking-wide">
                 Featured Project
               </p>
-              <h3 className="text-2xl font-bold">Course Reveal</h3>
+              <h3 className="text-2xl font-bold">
+                Vector Based Spotify Reccomendation Engine
+              </h3>
               <p className="bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md">
-                A feature-rich platform that allows students to view their
-                current and past classmates, fostering a sense of community and
-                collaboration within the university.
+                An innovative music recommendation engine that utilizes vector
+                databases and machine learning to analyze users' emotional
+                responses to song lyrics, offering personalized playlists based
+                on mood and sentiment.
               </p>
               <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
-                <li>React.js</li>
-                <li>TypeScript</li>
-                <li>Express.js</li>
-                <li>SQL</li>
-                <li>AWS</li>
+                <li>Python</li>
+                <li>Flask</li>
+                <li>Pinecone</li>
+                <li>Spotify API</li>
+                <li>HTML</li>
+                <li>JavaScript</li>
+                <li>CSS</li>
               </ul>
               <div className="text-2xl flex gap-4">
                 {/* Fill the href with the link to the project github*/}
-                <a href="" target="_blank">
+                <a href="https://github.com/Varun-Gangavarapu/VectorDatabaseMoodAnalysis" target="_blank">
                   <span
                     className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer 
                                 hover:-translate-y-2 transition-all duration-300"
@@ -57,14 +61,14 @@ const Projects = () => {
                   </span>
                 </a>
                 {/* Fill the href with the link to the project */}
-                <a href="" target="_blank">
+                {/* <a href="" target="_blank">
                   <span
                     className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer 
                                 hover:-translate-y-2 transition-all duration-300"
                   >
                     <RxOpenInNewWindow />
                   </span>
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
@@ -84,26 +88,27 @@ const Projects = () => {
                   src={project2}
                   alt="Project 2 Image"
                 />
-              <div className="hidden lgl:inline-block absolute w-full h-full bg-imgCover/40 rounded-md top-0 left-0 group-hover:bg-transparent duration-300"></div>
-
+                <div className="hidden lgl:inline-block absolute w-full h-full bg-imgCover/40 rounded-md top-0 left-0 group-hover:bg-transparent duration-300"></div>
               </div>
             </a>
             <div className="w-full h-4/5 mt-7 xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right  z-10">
               <p className="font-titleFont text-textGreen text-sm tracking-wide">
                 Featured Project
               </p>
-              <h3 className="text-2xl font-bold">Sudoku Royale</h3>
+              <h3 className="text-2xl font-bold">Course Reveal</h3>
               <p className="bg-[#112240] text-sm md:text-base p-2 xl:-mr-16 md:p-6 rounded-md">
-                A multiplayer Sudoku game with a competitive element,
-                incorporating accuracy points and trap purchases to enhance
-                gameplay.
+                A digital platform that connects students by allowing them to
+                sign in and discover peers who have taken the same classes,
+                encompassing a vast database of over 10,000 courses.
               </p>
               <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
+                <li>Next.js</li>
+                <li>React.js</li>
                 <li>TypeScript</li>
-                <li>Express.js</li>
-                <li>Socket.io</li>
-                <li>HTML</li>
-                <li>CSS</li>
+                <li>Tailwind.css</li>
+                <li>Python</li>
+                <li>Flask</li>
+                <li>AWS</li>
               </ul>
               <div className="text-2xl flex gap-4">
                 {/* Fill the href with the link to the project github*/}
--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,7 +1,7 @@
 import logo from "./images/logo.png"; 
 import profileImg from "./images/profile.png";
-import project1 from "./images/project1.png"
-import project2 from "./images/project2.png"
+import project1 from "./images/VectorDatabase.png"
+import project2 from "./images/CourseReveal.png"
 
 
 export { logo, profileImg, project1, project2 };
